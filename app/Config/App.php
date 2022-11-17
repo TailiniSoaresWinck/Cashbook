250a1b6e40c81ae5aa8b754f67568b4413c6fd43
TailiniSoaresWinck <tailini-swinck@educar.rs.gov.br>
2022-11-17 08:43:18 -0300

    modified:   app/Config/App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = '';
+    public $baseURL = 'https://emissaorelatorio.herokuapp.com';
 
     /**
      * --------------------------------------------------------------------------
